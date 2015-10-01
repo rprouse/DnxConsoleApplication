ad15cd8236db0d2de46e0576cd43ac9f98c25532
Rob Prouse <rob@prouse.org>
2015-10-01 10:45:47 -0400

    DNX console application for testing

---
+++ src/DnxConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+#if DNX451
+using Microsoft.Framework.Runtime;
+#else
+using Microsoft.Dnx.Runtime;
+#endif
+
+namespace DnxConsoleApp
+{
+    public class Program
+    {
+        public Program(ILibraryManager libraryManager)
+        {
+            foreach(var library in libraryManager.GetLibraries())
+            {
+                Console.WriteLine(library.Name);
+            }
+        }
+
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine();
+            Console.WriteLine("*** Press ENTER to Exit ***");
+            Console.ReadLine();
+        }
+    }
+}
